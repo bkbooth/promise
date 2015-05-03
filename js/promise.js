eec56b8c9b809c3d6784b840456bda460f1a48a5
Ben Booth <bkbooth@gmail.com>
2015-05-04 09:37:00 +1000

    Initial implementation
    * Handles a single success or failure case per-Promise

--- js/promise.js
+++ js/promise.js
@@ -1,8 +1,39 @@
 /**
  * Promise constructor
  *
+ * @param {function} deferred
+ *
  * @constructor
  */
-function Promise() {
-    // Nothing here yet
+function Promise(deferred) {
+    this._success = null;
+    this._failure = null;
+
+    deferred(
+        function resolve(value) {
+            if (this._success) {
+                this._success(value);
+            }
+        }.bind(this),
+        function reject(error) {
+            if (this._failure) {
+                this._failure(error);
+            }
+        }.bind(this)
+    )
 }
+
+/**
+ * Handle success and/or failure of deferred action
+ *
+ * @param {function} success
+ * @param {function} failure
+ */
+Promise.prototype.then = function(success, failure) {
+    if (success) {
+        this._success = success;
+    }
+    if (failure) {
+        this._failure = failure;
+    }
+};
