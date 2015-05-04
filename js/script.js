ecc8a49f8418f2a8a8cb7e6133cda183b751b56c
Ben Booth <bkbooth@gmail.com>
2015-05-04 17:30:37 +1000

    Update demo to use Promise.all() to fetch multiple images

--- js/script.js
+++ js/script.js
@@ -16,19 +16,31 @@ function init() {
 
     button.addEventListener('click', function clickListener() {
         setLoading(true);
-        Promise.race([
+        Promise.all([
+            fetch(imgUrl, { responseType: 'blob' }),
             fetch(imgUrl, { responseType: 'blob' }),
             fetch(imgUrl, { responseType: 'blob' })
         ]).catch(function(error) {
             console.error('Got no image!', error);
             console.info('Getting default image');
-            return fetch(defaultImgUrl, { responseType: 'blob' });
-        }).then(function(result) {
-            console.info('Got an image, create Object URL');
-            return URL.createObjectURL(result);
-        }).then(function(result) {
-            console.info('Got Object URL for image, apply to <img>.src');
-            img.src = result;
+            return Promise.all([ fetch(defaultImgUrl, { responseType: 'blob' }) ]);
+        }).then(function(results) {
+            console.info('Got images, create Object URLs');
+            return results.map(function(image) {
+                return URL.createObjectURL(image);
+            });
+        }).then(function(results) {
+            console.info('Got Object URLs for images, create and add new <img>s');
+            // Remove all previous images
+            Array.prototype.forEach.call(document.querySelectorAll('img'), function(img) {
+                img.parentNode.removeChild(img);
+            });
+            // Create and add new images
+            results.forEach(function(image) {
+                var img = document.createElement('img');
+                img.src = image;
+                document.querySelector('#images').appendChild(img);
+            });
         }).then(function() {
             console.info('Cleaning up');
             setLoading(false);
